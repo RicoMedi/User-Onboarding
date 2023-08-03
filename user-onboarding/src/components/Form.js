036ee149cdfb0640843c075aeb1bdf148d7a76ab
Madcodes831 <r.medina2119@gmail.com>
2023-08-02 18:37:08 -0700

    Initial set up

---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,28 @@
+import React from "react";
+ 
+
+
+
+
+
+const Form = (props) => {
+    return(
+<div>
+    <h1> My First Form </h1>
+        <form>
+            <label>
+                FirstName: &nbsp;
+                <input type="text"/>
+
+            </label>
+
+        </form>
+       
+       
+ </div>
+   
+    
+    )
+}
+
+export default Form;
